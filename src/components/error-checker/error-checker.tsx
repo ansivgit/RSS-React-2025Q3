d525d85c256c915258c4d5ac26fbf26a1475e14d
ansivgit <ansiv@yandex.ru>
2025-07-16 19:25:19 +0300

    fix: error-checker

--- src/components/error-checker/error-checker.tsx
+++ src/components/error-checker/error-checker.tsx
@@ -1,12 +1,31 @@
 import { Component } from 'react';
 import { Button } from '../button/button';
+import { ErrorIndicator } from '../error-indicator/error-indicator';
 
-export class ErrorChecker extends Component {
+type CheckerProps = unknown;
+export class ErrorChecker extends Component<
+  CheckerProps,
+  { hasError: boolean }
+> {
+  constructor(props: CheckerProps) {
+    super(props);
+
+    this.state = {
+      hasError: false,
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
   handleClick() {
+    this.setState({ hasError: true });
     throw new Error('Error from button');
   }
 
   render() {
+    if (this.state.hasError) {
+      return <ErrorIndicator />;
+    }
+
     return (
       <div className="d-grid gap-2 d-md-flex mb-3">
         <div className="col-6 mx-auto d-grid gap-2">
--- src/components/error-indicator/error-indicator.tsx
+++ src/components/error-indicator/error-indicator.tsx
@@ -1,8 +1,5 @@
 export const ErrorIndicator = () => (
-  <div className="alert alert-danger d-flex align-items-center" role="alert">
-    <svg className="bi flex-shrink-0 me-2" role="img" aria-label="Danger:">
-      <use xlinkHref="#exclamation-triangle-fill" />
-    </svg>
-    <div>An error occurred. Please try again later</div>
+  <div className="alert alert-danger" role="alert">
+    An error occurred. Please try again later
   </div>
 );
