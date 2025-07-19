c0101962668f2a5b8dd2efa1f4019f9903ea01f1
ansivgit <ansiv@yandex.ru>
2025-07-19 21:51:02 +0300

    fix: minor test-configs changes

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   test: {
     environment: 'jsdom',
     globals: true,
-    setupFiles: ['src/__tests__/setup-tests.tsx'],
+    setupFiles: ['src/__tests__/test-setup.ts'],
     exclude: [
       '**/node_modules/**',
       '**/dist/**',
